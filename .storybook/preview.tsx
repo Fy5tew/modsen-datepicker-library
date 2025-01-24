cc6cc9e4e2deaef521621df1bedbc3dec681bacb
Fy5tew <nikturchinovich@mail.ru>
2025-01-24 12:56:03 +0300

    feat: add providers to storybook preview

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,10 +1,24 @@
 import type { Preview } from '@storybook/react';
 
+import { FormattersProvider } from '#/contexts/formatters';
+import { IconsProvider } from '#/contexts/icons';
+import { ThemeProvider } from '#/contexts/theme';
+
 const preview: Preview = {
     parameters: {
         layout: 'centered',
     },
-    decorators: [(Story) => <Story />],
+    decorators: [
+        (Story) => (
+            <ThemeProvider>
+                <FormattersProvider>
+                    <IconsProvider>
+                        <Story />
+                    </IconsProvider>
+                </FormattersProvider>
+            </ThemeProvider>
+        ),
+    ],
 };
 
 export default preview;
